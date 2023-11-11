de0b7d4ab027c07fdd22daa63d45b351165f91e2
Akmalrbn <akmal.rabbani@ui.ac.id>
2023-11-11 13:08:13 +0700

    UI LOGIN

--- Client/app/src/main/java/com/rpl9/ratemykos/MainActivity.java
+++ Client/app/src/main/java/com/rpl9/ratemykos/MainActivity.java
@@ -6,22 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login = findViewById(R.id.buttonLogin);
-
-        login.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              Intent move = new Intent(MainActivity.this, LoginActivity.class);
-              startActivity(move);
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-
 }
