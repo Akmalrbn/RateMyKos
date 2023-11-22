c0776cd67b2295ec56666629f09c9e21bdef19e1
nevandaa <nevandafp@gmail.com>
2023-11-22 17:38:25 +0700

    test

--- Client/app/src/main/java/com/rpl9/ratemykos/KosDetailActivity.java
+++ Client/app/src/main/java/com/rpl9/ratemykos/KosDetailActivity.java
@@ -21,11 +21,10 @@ public class KosDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d("Listkos", "Masuk" + kosView.name);
         setContentView(R.layout.activity_kos_detail);
-        TextView name = findViewById(R.id.detailNameText);
+        TextView name = findViewById(R.id.detailKosTitle);
         TextView location = findViewById(R.id.detailLocationText);
 //        TextView size = findViewById(R.id.detailSizeText);
 //        TextView price = findViewById(R.id.detailPriceText);
-        TextView address = findViewById(R.id.detailAddText);
         CheckBox WiFi = findViewById(R.id.Wifi);
         CheckBox AC = findViewById(R.id.AC);
         CheckBox Bathroom = findViewById(R.id.Bathroom);
@@ -33,7 +32,6 @@ public class KosDetailActivity extends AppCompatActivity {
         CheckBox Kitchen = findViewById(R.id.Kitchen);
         name.setText(kosView.name);
         location.setText(kosView.location);
-        address.setText(kosView.latitude + ", " + kosView.longitude);
 
         if (kosView.facilities != null) {
             if (kosView.facilities.contains(Facility.WiFi)) {
